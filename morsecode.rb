61a7e2c8d8290b001b01184553fef86e20a9a4b8
moise <moisemlg90@gmail.com>
2022-06-10 12:53:35 +0200

    Fixing rubocop linters

--- morsecode.rb
+++ morsecode.rb
@@ -1,37 +1,36 @@
 # decode one character
-$morseCodes = {
-  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
-  '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
-  '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
-  '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
-  '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
-  '-.--' => 'Y', '--..' => 'Z'
+def decode_char(char)
+  morse_codes = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+    '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z'
   }
 
-
-def decode_char(char)  
-  $morseCodes[char]
+  morse_codes[char]
 end
 
 # decode a word
 def decode_word(word)
-  wordDecoded = ''
+  word_decoded = ''
   word.split.each do |letter|
-    wordDecoded += decode_char(letter)
+    word_decoded += decode_char(letter)
   end
-  wordDecoded
+  word_decoded
 end
 
 # decode the entire message
 def decode_message(message)
-  messageDecoded = ''
+  message_decoded = ''
   message.split('  ').each do |messages|
-    messageDecoded += "#{decode_word(messages)} "
+    message_decoded += "#{decode_word(messages)} "
   end
-  messageDecoded
+  message_decoded
 end
 
 puts 'Enter a message to decode!'
-getMessage = gets.chomp
-puts decode_message(getMessage)
+get_message = gets.chomp
+puts decode_message(get_message)
